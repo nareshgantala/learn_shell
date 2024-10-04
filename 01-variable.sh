5cfc668b5574cc4d4a8a6db2fbdc524caf6cc3d8
nareshgantala <nareshgantala@gmail.com>
2024-10-04 16:02:34 +0530

    test

--- 01-variable.sh
+++ 01-variable.sh
@@ -7,3 +7,10 @@ echo name = $name
 
 DATE=$(date +%F)
 echo today date is $DATE
+
+
+echo script name - $0
+echo first argument - $1
+echo second argument - $2
+echo all arguments - $*
+echo no of arguments - $#
